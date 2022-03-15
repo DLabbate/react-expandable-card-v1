67d83fb8121bad0ff3c97c45cfcecfc9297448f9
Domenic Labbate <domeniclabbate@gmail.com>
2022-03-14 22:21:34 -0400

    working on responsiveness

--- src/constants/data.js
+++ src/constants/data.js
@@ -6,7 +6,7 @@ export const properties = [
     price: "$1,200,000",
     address: "1204 Fake Address",
     description:
-      "Beautiful house . Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Large Patio.Garage Close to daycares, schools and accessible by public transport. 5 minutes by car from the Mega Centre Notre-Dame and all services. Quick access to highway 13/440 and REM. On the second floor: -The master bedroom is spacious . It has a balcony, a large walk-in closet, and its own en-suite bathroom. -The other three bedrooms are of good size and each has its own walk-in closet. -The basement is fully finished and offers a family room, as well as a game room . -The basement bedroom is convenient with a full bathroom.",
+      "Beautiful house! Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
     numBedroom: 4,
     numWashrooms: 3,
     livingSpace: "1,410 sq.ft.",
@@ -18,7 +18,7 @@ export const properties = [
     price: "$1,200,000",
     address: "1204 Fake Address",
     description:
-      "Beautiful house . Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Large Patio.Garage Close to daycares, schools and accessible by public transport. 5 minutes by car from the Mega Centre Notre-Dame and all services. Quick access to highway 13/440 and REM. On the second floor: -The master bedroom is spacious . It has a balcony, a large walk-in closet, and its own en-suite bathroom. -The other three bedrooms are of good size and each has its own walk-in closet. -The basement is fully finished and offers a family room, as well as a game room . -The basement bedroom is convenient with a full bathroom.",
+      "Beautiful house! Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
     numBedroom: 4,
     numWashrooms: 3,
     livingSpace: "1,410 sq.ft.",
@@ -30,7 +30,7 @@ export const properties = [
     price: "$1,200,000",
     address: "1204 Fake Address",
     description:
-      "Beautiful house . Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Large Patio.Garage Close to daycares, schools and accessible by public transport. 5 minutes by car from the Mega Centre Notre-Dame and all services. Quick access to highway 13/440 and REM. On the second floor: -The master bedroom is spacious . It has a balcony, a large walk-in closet, and its own en-suite bathroom. -The other three bedrooms are of good size and each has its own walk-in closet. -The basement is fully finished and offers a family room, as well as a game room . -The basement bedroom is convenient with a full bathroom.",
+      "Beautiful house! Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
     numBedroom: 4,
     numWashrooms: 3,
     livingSpace: "1,410 sq.ft.",
