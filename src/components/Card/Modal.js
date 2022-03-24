488210e91d888069340d555a9221e2eda19981ff
Domenic Labbate <domeniclabbate@gmail.com>
2022-03-23 21:46:31 -0400

    styling close button

--- src/components/Card/Modal.js
+++ src/components/Card/Modal.js
@@ -67,8 +67,11 @@ const Modal = ({ data }) => {
         <motion.div className="modal__row" variants={modalRowVariants}>
           <p className="modal__description">{description}</p>
         </motion.div>
-        <motion.div className="modal__close" whileHover={{ scale: 1.2 }}>
-          <IoCloseCircleOutline className="modal__close" />
+        <motion.div
+          className="modal__close-container"
+          whileHover={{ scale: 1.2 }}
+        >
+          <IoCloseCircleOutline className="modal__close-icon" />
         </motion.div>
       </motion.div>
     </motion.div>
