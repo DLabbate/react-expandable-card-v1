a12b9f67924ec2be07b2e21ae665ef891945fdaf
Domenic Labbate <domeniclabbate@gmail.com>
2022-04-18 17:30:26 -0400

    part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import "./App.css";
-import Card from "./components/Card";
-import { properties } from "./constants/data";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="properties">
-        {properties.map((item) => (
-          <Card data={item} />
-        ))}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
