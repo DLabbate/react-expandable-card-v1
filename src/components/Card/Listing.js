e6efae6b79b96d82ae379974b4fc614e68e09020
Domenic Labbate <domeniclabbate@gmail.com>
2022-04-22 21:03:59 -0400

    part-4

--- src/components/Card/Listing.js
+++ src/components/Card/Listing.js
@@ -2,7 +2,31 @@ import React from "react";
 import "./Listing.css";
 
 const Listing = ({ data }) => {
-  return <div className="listing"></div>;
+  const { imageUrl, price, address, numBedroom, numWashrooms, livingSpace } =
+    data;
+
+  return (
+    <div className="listing">
+      <div className="listing__content">
+        <div className="listing__image-container">
+          <img
+            className="listing__image"
+            alt="real estate mansion"
+            src={imageUrl}
+          />
+        </div>
+        <div className="listing__details">
+          <div className="listing__type">For Sale</div>
+          <div className="listing__row">
+            <span className="listing__price">{price}</span>
+          </div>
+          <div className="listing__row">
+            <span className="listing__address">{address}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Listing;
